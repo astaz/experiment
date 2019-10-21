1b85259e145725952a0d3c5d06f051781b40479c
talha <rtalha813@gmail.com>
2019-10-21 09:00:34 -0700

    Initial commit

---
+++ app/src/main/java/com/example/backgroundchange/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.backgroundchange;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button=findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ResourceAsColor")
+            @Override
+            public void onClick(View view) {
+
+                RelativeLayout rl = findViewById(R.id.layout);
+                rl.setBackgroundResource(R.drawable.background);
+
+            }
+        });
+
+    }
+}
