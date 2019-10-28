cde17f1f00ce3ca3a0958f6a5801c968c214303a
talha <rtalha813@gmail.com>
2019-10-28 08:15:02 -0700

    k

--- app/src/main/java/com/example/backgroundchange/MainActivity.java
+++ app/src/main/java/com/example/backgroundchange/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 Button button;
-String nameChangeKeyaHakk;
+String nameChangeKeyaHakk0k;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
